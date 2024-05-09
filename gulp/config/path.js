33716e5f7b8e74b6ee4de6ec3672f9d4c065d3ef
Alexander <kanashkin_ad@mail.ru>
2024-05-09 21:35:58 +0300

    add changes

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -17,7 +17,7 @@ const path = {
         js: `${srcFolder}/js/app.js`,
         images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
         svg: `${srcFolder}/img/**/*.svg`,
-        scss: `${srcFolder}/scss/style.scss`,
+        scss: `${srcFolder}/scss/*.scss`,
         html: `${srcFolder}/*.html`,
         files: `${srcFolder}/files/**/*.*`,
     },
